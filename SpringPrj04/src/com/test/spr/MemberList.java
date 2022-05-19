8bfc80994053f60978f38e2bc6746ee4c532fcfd
MinseongKimz <minseongkimc@gmail.com>
2022-05-19 18:44:32 +0900

    2022-05-19 수업내용 for framework

---
+++ SpringPrj04/src/com/test/spr/MemberList.java
@@ -0,0 +1,59 @@
+/*
+   MemberList.java
+   - DAO 클래스를 참조하는 클래스
+   - 의존관계 설정
+   - print() 메소드 구현 
+*/
+package com.test.spr;
+
+public class MemberList
+{
+	// ※ 스프링 IoC/DI 패턴 적용
+	// check~!!
+	// 멤버 변수 추가하는 과정에서 데이터 타입은 인터페이스 형식
+	// DI 를 위한 준비 작업
+	private IDAO dao;
+	
+	// check
+	// setter 메소드 추가
+	public void setDao(IDAO dao)
+	{
+		this.dao = dao;
+	}
+	
+	
+  
+   // print() 메소드 구현
+   public void print()
+   {
+      try
+      {
+         for (MemberDTO dto : dao.lists())
+         {
+            System.out.printf("%10s %4s %15s %15s%n"
+                          , dto.getId(), dto.getName()
+                          , dto.getTel(), dto.getEmail());
+            
+         }
+         
+         
+      } catch (Exception e)
+      {
+         System.out.println(e.toString());
+      }
+   }
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
---
+++ SpringPrj04/src/com/test/spr/OracleDAO.java
@@ -0,0 +1,54 @@
+/*==========================
+	OracleDAO.java
+	- MssqlDAO 와 설정만 다르게 연결하여 실습
+	- MSSQL 안깔려있음
+	- IDAO 인터페이스를 구현하는 클래스
+===========================*/
+package com.test.spr;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OracleDAO implements IDAO
+{
+	@Override
+	public List<MemberDTO> lists() throws ClassNotFoundException, SQLException
+	{
+		List<MemberDTO> result = new ArrayList<MemberDTO>();
+		
+		// db 연결
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@211.238.142.163:1521:xe", "scott","tiger" );
+		
+		// 쿼리문 준비
+		String sql = "SELECT ID, NAME, TEL, EMAIL FROM TBL_MEMBERLIST";
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		while (rs.next())
+		{
+			MemberDTO dto = new MemberDTO();
+			
+			dto.setId(rs.getString("ID"));
+			dto.setName(rs.getString("NAME"));
+			dto.setTel(rs.getString("TEL"));
+			dto.setEmail(rs.getString("EMAIL"));
+			
+			result.add(dto);
+		}
+			
+		// 리소스 반납
+		rs.close();
+		stmt.close();
+		
+		if(!conn.isClosed())
+			conn.close();
+		
+		return  result;
+	}
+}
